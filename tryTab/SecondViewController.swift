87ec370fb916ad1451b7dbd73b574ca72b2d8ff6
yingshi2502 <huangyingshi@gmail.com>
2019-09-27 21:06:18 +0800

    first commit

---
+++ tryTab/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tryTab
+//
+//  Created by yingshi on 27/9/19.
+//  Copyright © 2019 huangyingshi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
